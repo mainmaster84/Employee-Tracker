5c2467d836bf61e97ca3091bef6998672a9c8cbe
Ronald Main <rbmain84@yahoo.com>
2021-06-11 11:35:06 -0500

    update files

--- server.js
+++ server.js
@@ -1,27 +1,16 @@
-const express = require('express');
-const db = require('./db/connection');
-const apiRoutes = require('./routes/apiRoutes');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// Use apiRoutes
-app.use('/api', apiRoutes);
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
+// Connect to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: '',
+  // Your MySQL password
+  password: '',
+  database: 'tracker'
 });
 
-// Start server after DB connection
-db.connect(err => {
+connection.connect(function(err) {
   if (err) throw err;
-  console.log('Database connected.');
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
 });
